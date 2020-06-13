a846e56a5b663defa9c10fb813c4fb80153fcd67
kocelly <kocelly16@gmail.com>
2020-06-13 18:28:26 -0500

    Se agregaron interfaces y se migro al paq main

---
+++ src/main/IAbstractFactory.java
@@ -0,0 +1,6 @@
+package main;
+
+public interface IAbstractFactory {
+    IComputadora crearComputadora();
+    ITablet crearTablet();
+}
---
+++ src/main/ITablet.java
@@ -0,0 +1,9 @@
+package main;
+
+/**
+ *
+ * @author Dell
+ */
+public interface ITablet {
+    
+}
